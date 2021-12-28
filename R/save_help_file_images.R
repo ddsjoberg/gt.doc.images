d5bb1a65fb15686d9d3e283f05e286afda6c45ae
Daniel Sjoberg <danield.sjoberg@gmail.com>
2021-12-28 08:37:05 -0500

    Update save_help_file_images.R

--- R/save_help_file_images.R
+++ R/save_help_file_images.R
@@ -7,10 +7,13 @@
 #' @param pkg Name of package
 #' @param rd_files Character vector of Rd file names to search. Default is all
 #' Rd files.
+#' @inheritParams utils::example
 #'
 #' @export
 
-save_help_file_images <- function(pkg, rd_files = NULL, delete_existing_pngs =  FALSE) {
+save_help_file_images <- function(pkg, rd_files = NULL,
+                                  delete_existing_pngs =  FALSE,
+                                  run.dontrun = TRUE, run.donttest = TRUE) {
   # list of all help files
   if (is.null(rd_files)) {
     rd_files <-
@@ -34,59 +37,66 @@ save_help_file_images <- function(pkg, rd_files = NULL, delete_existing_pngs =  
   # cycling over each help file, and saving gt images
   gtsummary::set_gtsummary_theme(list("pkgwide-lgl:quiet" = TRUE))
   for (f in rd_files) {
-    cli::cli_alert_success("Working on {f}")
+    cli::cli_alert_success("Working on {.field {f}}")
 
     # run code from example
-    utils::example(topic = f, package = pkg, character.only = TRUE,
-                   give.lines = FALSE, echo = FALSE)
+    suppressWarnings(
+      utils::example(topic = f, package = pkg, character.only = TRUE,
+                     give.lines = FALSE, echo = FALSE, local = FALSE,
+                     run.dontrun = run.dontrun, run.donttest = run.donttest)
+    )
 
     # get list of example objects that end in "_ex###"
     example_objs <-
-      ls()[stringr::str_ends(ls(), "_ex[:digit:]+") | stringr::str_ends(ls(), "_ex")]
+      ls(envir = rlang::global_env())[stringr::str_ends(ls(envir = rlang::global_env()), "_ex[:digit:]+") | stringr::str_ends(ls(envir = rlang::global_env()), "_ex")]
 
-    # saving an image of every gt, gtsummary, or flextable example
-    purrr::walk(
-      example_objs,
-      function(example_chr) {
-        # converting string to object
-        example_obj <- eval(parse(text = example_chr))
-        cli::cli_alert_success("  Saving `{example_chr}.png`")
+    if (rlang::is_empty(example_objs))
+      cli::cli_alert_success("  No Example Objects Present in {.field {f}}")
+    else {
+      # saving an image of every gt, gtsummary, or flextable example
+      purrr::walk(
+        example_objs,
+        function(example_chr) {
+          # converting string to object
+          example_obj <- eval(parse(text = example_chr))
+          cli::cli_alert_success("  Saving '{example_chr}.png'")
 
-        # convert gtsummary object to gt
-        if (inherits(example_obj, "gtsummary")) {
-          example_obj <- gtsummary::as_gt(example_obj)
+          # convert gtsummary object to gt
+          if (inherits(example_obj, "gtsummary")) {
+            example_obj <- gtsummary::as_gt(example_obj)
+          }
+
+          # checking object is now a gt object
+          if (inherits(example_obj, "gt_tbl")) {
+            # saving image
+            gt::gtsave(example_obj,
+                       filename = here::here("man", "figures", stringr::str_glue("{example_chr}.png"))
+            )
+          }
+
+          # saving flextable image
+          if (inherits(example_obj, "flextable")) {
+            flextable::save_as_image(example_obj,
+                                     webshot = "webshot2",
+                                     path = here::here("man", "figures", stringr::str_glue("{example_chr}.png"))
+            )
+          }
+
+          # shrink image
+          shrink_it <-
+            tryCatch(
+              utils::capture.output(
+                webshot::shrink(here::here("man", "figures", stringr::str_glue("{example_chr}.png")))
+              ),
+              error = function(e) {
+                return(invisible())
+              }
+            )
         }
-
-        # checking object is now a gt object
-        if (inherits(example_obj, "gt_tbl")) {
-          # saving image
-          gt::gtsave(example_obj,
-                     filename = here::here("man", "figures", stringr::str_glue("{example_chr}.png"))
-          )
-        }
-
-        # saving flextable image
-        if (inherits(example_obj, "flextable")) {
-          flextable::save_as_image(example_obj,
-                                   webshot = "webshot2",
-                                   path = here::here("man", "figures", stringr::str_glue("{example_chr}.png"))
-          )
-        }
-
-        # shrink image
-        shrink_it <-
-          tryCatch(
-            utils::capture.output(
-              webshot::shrink(here::here("man", "figures", stringr::str_glue("{example_chr}.png")))
-            ),
-            error = function(e) {
-              return(invisible())
-            }
-          )
-        return(invisible())
-      }
-    )
+      )
+      rm(list = example_objs, envir = rlang::global_env())
+    }
   }
   gtsummary::reset_gtsummary_theme()
-
+  return(invisible())
 }
